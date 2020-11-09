b383bad3ddadca06c8360b89911025204b5db87d
Francisco Claros <69797982+franciscoclaros@users.noreply.github.com>
2020-11-09 12:58:02 +0100

    Create mainpd.tf

---
+++ mainpd.tf
@@ -0,0 +1,17 @@
+#Config Docker provider
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+resource "docker_container" "jenkins" {
+    image = "${docker_image.jenkins.latest}"
+    name = "terraform-demo"
+    ports = {
+       internal = 8080
+       external = 8080
+       internal = 5000
+       external = 5000
+       }
+ }
+resource "docker_image" "jenkins" {
+    name = "jenkins:latest"
+}
